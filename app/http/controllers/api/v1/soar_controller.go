afc4fc52f3f2afb43650f5ab60d8e839d7f6110c
jwwww <365039476@qq.com>
2022-08-22 23:45:25 +0800

    拆分

--- app/http/controllers/api/v1/soar_controller.go
+++ app/http/controllers/api/v1/soar_controller.go
@@ -2,11 +2,10 @@ package v1
 
 import (
 	"go_web/app/requests"
+	"go_web/app/services"
 	"go_web/pkg/config"
 	"go_web/pkg/response"
 	"os"
-	"os/exec"
-	"runtime"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -20,29 +19,19 @@ type SoarController struct {
 func (ctrl *SoarController) File(c *gin.Context) {
 	rootPath, _ := os.Getwd()
 	//处理文件上传
-	file, errLoad := c.FormFile("file")
+	sqlPath, errLoad := services.UploadFile(c, rootPath)
 	if errLoad != nil {
 		response.BadRequest(c, errLoad, "")
-	}
-	ret := make(map[string]string)
-	ret["file_name"] = cast.ToString(time.Now().Unix()) + "_" + file.Filename
-	ret["file_name_origin"] = file.Filename
-	sqlPath := rootPath + "/" + cast.ToString(config.Env("SOAR_SQL")) + ret["file_name"]
-
-	err := c.SaveUploadedFile(file, sqlPath)
-	if err != nil {
-		response.BadRequest(c, err, "")
 	}
 
 	//开始执行分析并保存结果
 	randFileName := cast.ToString(time.Now().Unix()) + ".html"
 	soarResultPath := rootPath + "/" + cast.ToString(config.Env("SOAR_RESULT")) + "/" + randFileName
-	// {
-	err1 := AnalyzeAndSave(rootPath, sqlPath, soarResultPath)
-	if err1 != nil {
-		response.BadRequest(c, err1, "")
+
+	errAnalyze := services.AnalyzeAndSave(rootPath, sqlPath, soarResultPath)
+	if errAnalyze != nil {
+		response.BadRequest(c, errAnalyze, "")
 	}
-	// }
 
 	//ajax 返回
 	url := cast.ToString(config.Env("APP_URL")) + "/soar-result/" + randFileName
@@ -69,7 +58,7 @@ func (ctrl *SoarController) Sql(c *gin.Context) {
 	randFileName := cast.ToString(time.Now().Unix()) + ".html"
 	soarResultPath := rootPath + "/" + cast.ToString(config.Env("SOAR_RESULT")) + "/" + randFileName
 	//执行分析并保存结果
-	err := AnalyzeAndSave(rootPath, sqlPath, soarResultPath)
+	err := services.AnalyzeAndSave(rootPath, sqlPath, soarResultPath)
 	if err != nil {
 		response.BadRequest(c, err, "")
 	}
@@ -82,35 +71,4 @@ func (ctrl *SoarController) Sql(c *gin.Context) {
 	response.Data(c, 200, gin.H{
 		"data": data,
 	})
-}
-
-//获取执行文件路径
-func GetSoarPath(rootPath string) string {
-	var soarPath string
-	switch os := runtime.GOOS; os {
-	case "windows":
-		soarPath = rootPath + "/soar/soar.windows-amd64"
-	case "linux":
-		soarPath = rootPath + "/soar/soar.windows-amd64"
-	default:
-		//其他系统
-	}
-	return soarPath
-}
-
-func AnalyzeAndSave(rootPath string, sqlPath string, soarResultPath string) error {
-	//获取执行文件的路径
-	soarPath := GetSoarPath(rootPath)
-	//开始执行分析
-	cmd := exec.Command(soarPath, "-query", sqlPath)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return err
-	}
-	//结果保存
-	os.WriteFile(soarResultPath, []byte(string(out)), 0644)
-	//清理残余的临时库表
-	defer exec.Command(soarPath, "--cleanup-test-database")
-
-	return nil
 }
---
+++ app/services/soar_service.go
@@ -0,0 +1,62 @@
+package services
+
+import (
+	"go_web/pkg/config"
+	"os"
+	"os/exec"
+	"runtime"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cast"
+)
+
+//使用 soar 分析并保存结果
+func AnalyzeAndSave(rootPath string, sqlPath string, soarResultPath string) error {
+	//获取执行文件的路径
+	soarPath := GetSoarPath(rootPath)
+	//开始执行分析
+	cmd := exec.Command(soarPath, "-query", sqlPath)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+	//结果保存
+	os.WriteFile(soarResultPath, []byte(string(out)), 0644)
+	//清理残余的临时库表
+	defer exec.Command(soarPath, "--cleanup-test-database")
+
+	return nil
+}
+
+//获取执行文件路径
+func GetSoarPath(rootPath string) string {
+	var soarPath string
+	switch os := runtime.GOOS; os {
+	case "windows":
+		soarPath = rootPath + "/soar/soar.windows-amd64"
+	case "linux":
+		soarPath = rootPath + "/soar/soar.windows-amd64"
+	default:
+		//其他系统
+	}
+	return soarPath
+}
+
+//处理文件上传
+func UploadFile(c *gin.Context, rootPath string) (string, error) {
+	file, errLoad := c.FormFile("file")
+	if errLoad != nil {
+		return "", errLoad
+	}
+	ret := make(map[string]string)
+	ret["file_name"] = cast.ToString(time.Now().Unix()) + "_" + file.Filename
+	ret["file_name_origin"] = file.Filename
+	sqlPath := rootPath + "/" + cast.ToString(config.Env("SOAR_SQL")) + ret["file_name"]
+
+	err := c.SaveUploadedFile(file, sqlPath)
+	if err != nil {
+		return "", err
+	}
+	return sqlPath, nil
+}

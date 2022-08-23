6be89fe24eae04e1db4bfe4371be4beccb3a38a1
jwwww <365039476@qq.com>
2022-08-24 00:44:50 +0800

    bootstrap

--- routes/api.go
+++ routes/api.go
@@ -3,13 +3,25 @@ package routes
 
 import (
 	controllers "go_web/app/http/controllers/api/v1"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 // RegisterAPIRoutes 注册网页相关路由
 func RegisterAPIRoutes(r *gin.Engine) {
-
+	//静态页面
+	r.LoadHTMLGlob("html/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "go-soar",
+		})
+	})
+	r.GET("/file", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "file.html", gin.H{
+			"title": "go-soar",
+		})
+	})
 	//v1 的路由组
 	v1 := r.Group("/v1")
 	{
